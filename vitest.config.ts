f6f359684fa3785681f53ca774c0c029f7b6a9a7
Edwin Isac <edwinisac007@gmail.com>
2026-04-21 07:36:55 +0100

    chore(phase2): install dexie, zustand, fake-indexeddb

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     globals: true,
     passWithNoTests: true,
     exclude: ['**/node_modules/**', '**/tests/e2e/**'],
-    setupFiles: ['./src/test-setup.ts'],
+    setupFiles: ['./src/test-setup.ts', 'fake-indexeddb/auto'],
     alias: { '@': resolve(__dirname, './src') },
     coverage: {
       provider: 'v8',
